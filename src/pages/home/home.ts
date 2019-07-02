b14abc7c1d6ec059de0592b8698c2f52524cbf91
CodeName-Spaz <spazgrafficz@outlook.com>
2019-07-02 13:19:51 +0200

    Changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,8 +12,19 @@ export class HomePage {
 
   }
 
-displayOrganizatins(){
-  
-}
-  
+  displayOrganizatins() {
+
+  }
+  m = 0
+  showMoreDetails() {
+    var detailsOrg = document.getElementById("misc");
+    if(this.m == 0){
+      this.m = 1;
+      detailsOrg.style.maxHeight = "1000px"
+    }
+    else{
+      this.m = 0;
+      detailsOrg.style.maxHeight = "35px"
+    }
+  }
 }
