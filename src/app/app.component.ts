88a6be3bc3525c49f5695af70e44328e03b6ed13
CodeName-Spaz <spazgrafficz@outlook.com>
2019-07-21 13:13:02 +0200

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class MyApp {
           this.rootPage = HomePage
         }
         else {
-          this.rootPage = AddOrganizationPage
+          this.rootPage = LoginRegisterPage
         }
        })
       // Okay, so the platform is ready and our plugins are available.
