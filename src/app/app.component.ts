7e81c6fa3c0d3599bcc184bc4d5ad25814490998
Mxolisi Nkosinathi Nene <mxolisinkosinathinene@Regenesyss-MacBook-Air-3.local>
2019-07-24 14:17:31 +0200

    mxolisi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class MyApp {
           this.rootPage = HomePage
         }
         else {
-          this.rootPage = LoginRegisterPage
+          this.rootPage = OnboardingPage
         }
        })
       // Okay, so the platform is ready and our plugins are available.
